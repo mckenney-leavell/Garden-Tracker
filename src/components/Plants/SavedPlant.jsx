31218134a53113b10d946bedeb52bf1389ed115f
McKenney Leavell <maleavell@gmail.com>
2025-09-17 09:54:33 -0500

    plant save functionality completed

--- src/components/Plants/SavedPlant.jsx
+++ src/components/Plants/SavedPlant.jsx
@@ -2,22 +2,13 @@ import "./Plant.css"
 
 function SavedPlant( {plant} ) {
     return (
-        // <section className="plant">
-        //     <div className="plant-image">
-        //         <img src={plant.imageURL} alt={plant.name}/>
-        //     </div>
-        //     <div className="plant-info">
-        //         <div>{plant.name}</div>
-        //         <button className="save-to-garden-btn">Save</button>
-        //     </div>
-        // </section>
         <section className="plant">
             <div className="plant-image">
                 <img src={plant.plant.imageURL} alt={plant.plant.name}/>
             </div>
             <div className="plant-info">
                 <div>{plant.plant.name}</div>
-                <button className="save-to-garden-btn">Save</button>
+                <button className="remove-from-garden-btn">Remove</button>
             </div>
         </section>
     )
